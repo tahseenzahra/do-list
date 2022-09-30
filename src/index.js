de771ad5ea22d384cf480451ad0e0b8ec4808c39
tahseenzahra <tahseen1zahra@gmail.com>
2022-09-30 21:42:12 +0500

    Remove commented code

--- src/index.js
+++ src/index.js
@@ -51,14 +51,3 @@ function updateTaskListToHTML() {
 }
 
 updateTaskListToHTML();
-
-// function component() {
-//   const element = document.createElement('div');
-
-//   // Lodash, now imported by this script
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//   element.tagName.add(body);
-//   return element;
-// }
-
-// document.body.appendChild(component());
