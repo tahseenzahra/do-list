94f16b72f71569bd438bb3ef2796c4a7ebd027dc
tahseenzahra <tahseen1zahra@gmail.com>
2022-09-30 21:21:19 +0500

    Add basic structure of todo list

--- src/index.js
+++ src/index.js
@@ -1,13 +1,65 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const element = document.createElement('div');
+import dragDropIcon from './images/drag-drop-icon.svg';
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.tagName.add(body);
-  return element;
+const toDoTask = [
+  {
+    description: 'task1',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'task2',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'task3',
+    completed: false,
+    index: 3,
+  },
+];
+
+function getTaskList(task) {
+  const toDoTaskItem = document.createElement('li');
+  toDoTaskItem.classList.add('todo-list-item');
+  {
+    const toDoTaskStatus = document.createElement('input');
+    toDoTaskStatus.type = 'checkbox';
+    toDoTaskStatus.checked = task.completed;
+    toDoTaskStatus.classList.add('task-status');
+    toDoTaskItem.appendChild(toDoTaskStatus);
+  }
+  {
+    const toDoTaskDescription = document.createElement('p');
+    toDoTaskDescription.textContent = task.description;
+    toDoTaskDescription.classList.add('task-description');
+    toDoTaskItem.appendChild(toDoTaskDescription);
+  }
+  {
+    const toDoTaskplaceicon = document.createElement('img');
+    toDoTaskplaceicon.src = dragDropIcon;
+    toDoTaskplaceicon.checked = task.completed;
+    toDoTaskplaceicon.classList.add('drop-drag-icon');
+    toDoTaskItem.appendChild(toDoTaskplaceicon);
+  }
+  return toDoTaskItem;
 }
 
-document.body.appendChild(component());
+function updateTaskListToHTML() {
+  toDoTask.forEach((task) => document.getElementById('todo-list').appendChild(getTaskList(task)));
+}
+
+updateTaskListToHTML();
+
+// function component() {
+//   const element = document.createElement('div');
+
+//   // Lodash, now imported by this script
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//   element.tagName.add(body);
+//   return element;
+// }
+
+// document.body.appendChild(component());
